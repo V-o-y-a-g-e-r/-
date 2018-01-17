3f6674c039753ec832b479eb5466af878cf2944f
V-o-y-a-g-e-r <35385484+V-o-y-a-g-e-r@users.noreply.github.com>
2018-01-17 22:11:51 +0100

    Edit Paint

--- Graphics_Application/main_f.py
+++ Graphics_Application/main_f.py
@@ -5,59 +5,43 @@ class Application(tk.Tk):
     def __init__(self):#class constructor
 
         tk.Tk.__init__(self)#parent class constructor
-        ###
+        #window parameters
         self.title("Window Application")
         self.geometry("500x404")
         self.resizable(0,0)
-        ###
-        self.menu = tk.Menu(self)#menu bar
-        self.file_menu = tk.Menu(self.menu, tearoff = 0)
-        #file menu bar which contains cascade
-        self.color_menu = tk.Menu(self.file_menu, tearoff = 0)
-        self.menu.add_cascade(label = "Edit", menu = self.file_menu)
-        #adding cascade to the bar
-        self.file_menu.add_cascade(label = "Color choose", menu = self.color_menu)
-        self.file_menu.add_command(label = "Draw line", command = self.draw_line)
-        self.color_menu.add_command(label = "Blue", command = lambda: self.color_change("blue"))
-        self.color_menu.add_command(label="Red", command= lambda: self.color_change("red"))
-        self.color = "black"  # default color is black
-        #canvas widget filed
-        self.w = tk.Canvas(self, width = self.winfo_reqwidth(), height = self.winfo_reqheight()*2, bg = "white")
-        self.w.pack(fill = tk.BOTH)
-        self.w.bind("<B1-Motion>", self.paint)
-        self.w.bind("<B3-Motion>", self.rubber)
-        #now, define coordinates for other drawings
-        self.x1, self.y1, self.x2, self.y2 = None, None, None, None
-        self.w.bind("<Button-1>", self.first_coords)
-        self.w.bind("<Button-3>", self.second_coords)
+        #canvas
+        self.paint_here = tk.Canvas(self, width = self.winfo_reqwidth(), height = self.winfo_reqheight()*2, bg = "white")
+        self.paint_here.pack(fill = tk.BOTH)
+        #locations for drawing stuff
+        self.x1, self.y1 = None, None
+        self.paint_obj = "Line"
+        #binding buttons for drawing
+        self.paint_here.bind("<B1-Motion>", self.motion)
+        self.paint_here.bind("<ButtonPress-1>", self.button_down)
+        self.paint_here.bind("<ButtonRelease-1>", self.button_up)
 
-        self.config(menu = self.menu)
-
-    def first_coords(self, event):
+    def button_down(self, event):
+        #take coords when button is down
         self.x1, self.y1 = event.x, event.y
 
-    def second_coords(self, event):
-        self.x2, self.y2 = event.x, event.y
+    def button_up(self, event):
+        #delete coords
+        self.x1, self.y1 = None, None
 
-    def draw_line(self):
-        self.w.create_line(self.x1, self.y1, self.x2, self.y2, fill = self.color)
+    def motion(self, event):
+        #when button is down and moving
+        #here, functions for drawing will be used
+        if self.paint_obj == "Line":
+            self.create_line(event)
 
-    #paint method
-    def paint(self, event):
-        x1, y1 = event.x -1, event.y -1
-        x2, y2 = event.x +1, event.y +1
-        self.w.create_oval(x1, y1, x2, y2, fill = self.color, outline=self.color)
 
-    def rubber(self, event):
-        x1, y1 = event.x - 10, event.y - 10
-        x2, y2 = event.x + 10, event.y + 10
-        self.w.create_oval(x1, y1, x2, y2, fill = "white", outline = "white")
+        #updating position of the mouse
+        self.x1, self.y1 = event.x, event.y
 
-    #color change method
-    def color_change(self, color):
-        #here, add other colors in the cascade
-        if color == "blue": self.color = "blue"
-        elif color == "red": self.color = "red"
+    def create_line(self, event):
+        #drawing line
+        event.widget.create_line(self.x1, self.y1, event.x, event.y,
+                                 smooth=tk.TRUE, fill="black", width=2)
 
 my_app = Application()
 my_app.mainloop()
